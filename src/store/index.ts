3a6ace50a3eeef240dec32aae50523f0a3c83552
Howard Gil <howardbgil@gmail.com>
2023-07-18 02:35:47 -0700

    removed preset filters from store to make checkboxes more user friendly

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,19 +6,10 @@ import { create } from "zustand"
 import { EventTypes, LocationTypes } from "../utils/constants"
 
 const useStore = create((set) => ({
-  eventTypeFilters: [
-    EventTypes.ALL,
-    EventTypes.CO_WORKING,
-    EventTypes.HACKATHON,
-  ],
+  eventTypeFilters: [EventTypes.ALL],
   setEventTypeFilters: (input: string[]) =>
     set((state: any) => ({ eventTypeFilters: input })),
-  locationTypeFilters: [
-    LocationTypes.ALL,
-    LocationTypes.SF,
-    LocationTypes.NYC,
-    LocationTypes.REMOTE,
-  ],
+  locationTypeFilters: [LocationTypes.ALL],
   setLocationTypeFilters: (input: string[]) =>
     set((state: any) => ({ locationTypeFilters: input })),
   dateFilter: undefined,
