b31415ac2c48cd6b595575aaf416503e9a09a0da
Howard Gil <howardbgil@gmail.com>
2023-07-19 22:27:33 -0700

    Added Airtable form for Submit Event

--- components/SubmitEvent.tsx
+++ components/SubmitEvent.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import Link from "next/link"
 
 import { Button } from "./ui/button"
 
@@ -6,7 +7,9 @@ export default function SubmitEvent() {
   return (
     <div className="flex justify-end">
       <Button className="flex h-fit w-fit justify-end px-2 text-xs">
-        Submit Event
+        <Link href="https://airtable.com/appNOa0dRcLXhHft1/shrhGUZFG8n4YAaU1">
+          Submit Event
+        </Link>
       </Button>
     </div>
   )
