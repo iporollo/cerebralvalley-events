4dfe62366d24b78d4b7f0d5550226494d0c2b2eb
Ivan Porollo <iporollo@gmail.com>
2023-08-08 21:02:19 -0700

    update google sheet link

--- config/site.ts
+++ config/site.ts
@@ -14,12 +14,7 @@ export const siteConfig: SiteConfig = {
   name: "Cerebral Valley",
   description:
     "A community of founders and builders creating the next generation of technology.",
-  mainNav: [
-    {
-      title: "Google Sheet",
-      href: "https://docs.google.com/spreadsheets/d/1P6ut7vL-gXKbeDeh3nuPqBjoCupjIt87Sw7TnhumBSU/edit#gid=1781893986",
-    },
-  ],
+  mainNav: [],
   links: {
     slack:
       "https://join.slack.com/t/cerebral-valley/shared_invite/zt-1x3ajihhs-XVyLxWO84uY7CCnShfI1cw",
