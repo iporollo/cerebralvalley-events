84dbfc61d72c9f1c631e42a12aa1cb91d9172f1a
Ivan Porollo <iporollo@gmail.com>
2023-09-08 01:53:51 -0700

    add google sheet link to mobile view

--- components/Timeline/TimelineRow.tsx
+++ components/Timeline/TimelineRow.tsx
@@ -1,10 +1,18 @@
 import DateComponent from "./DateComponent"
 import UpcomingEventCard from "./UpcomingEventCard"
 
-export default function TimelineRow({ event, showDateComponent }: { event: EventType, showDateComponent: boolean }) {
+export default function TimelineRow({
+  event,
+  showDateComponent,
+}: {
+  event: EventType
+  showDateComponent: boolean
+}) {
   return (
     <div className="flex-row border-l-2 border-dashed border-[#e8e8ea] pb-10 dark:border-[#28272c]">
-      {showDateComponent && <DateComponent startDate={event.startDate} endDate={event.endDate} /> }
+      {showDateComponent && (
+        <DateComponent startDate={event.startDate} endDate={event.endDate} />
+      )}
       <UpcomingEventCard event={event} />
     </div>
   )
